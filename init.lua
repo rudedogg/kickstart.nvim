f093fd48e1dfb4f93bc3eb2bf79451eef61a350b
Austin Rude <rudedogg@users.noreply.github.com>
2023-09-11 17:52:08 -0600

    Narrow down tree-sitter languages
    Doesn't matter but why not

--- init.lua
+++ init.lua
@@ -399,7 +399,8 @@ vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = 
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'zig' },
+  --ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'zig' },
+  ensure_installed = { 'c', 'cpp', 'vimdoc', 'vim', 'zig' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
